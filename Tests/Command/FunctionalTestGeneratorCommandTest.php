b5bb6edd397fa74b5d78a5c1d2ac31b2f3a58475
Charles-Antoine FOURNEL <charles@huitiemesens.fr>
2016-01-02 14:53:42 +0100

    upgrade tests , still fail

--- Tests/Command/FunctionalTestGeneratorCommandTest.php
+++ Tests/Command/FunctionalTestGeneratorCommandTest.php
@@ -1,27 +1,45 @@
 <?php
 namespace huitiemesens\FunctionalTestGeneratorBundle\Tests\Command;
-require_once __DIR__.'/../../vendor/autoload.php';
 
-use huitiemesens\functionalTestGeneratorbundle\Command\FunctionalTestGeneratorCommand;
-use Symfony\Component\Console\Application;
+
 use Symfony\Component\Console\Tester\CommandTester;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\NullOutput;
-//use Symfony\Component\DependencyInjection\Container;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use huitiemesens\FunctionalTestGeneratorBundle\Command\FunctionalTestGeneratorCommand;
 
-class FunctionalTestGeneratorCommandTest extends \PHPUnit_Framework_TestCase
+class FunctionalTestGeneratorCommandTest extends WebTestCase
 {
     private $command;
 
     public function testExecute()
     {
-        $application = new Application();
+        $kernel = $this->createKernel();
+        $kernel->boot();
+        $application = new Application($kernel);
         $application->add(new FunctionalTestGeneratorCommand());
 
         $command = $application->find('tests:generate');
+        //$this->mockCommandDialogHelper($command);
         $commandTester = new CommandTester($command);
-        $commandTester->execute(array('command' => $command->getName()));
-
+        $commandTester->execute(array(
+            'command'      => $command->getName(),
+            'bundle'         => 'huitiemesens:FunctionalTestGeneratorBundle'
+        ));
+        
+        echo $commandTester->getDisplay();
         $this->assertRegExp('/.../', $commandTester->getDisplay());
+    }
+
+    /**
+     * Mocking command input (not to show command prompt)
+     * @param Command $command
+     */
+    private function mockCommandDialogHelper(FunctionalTestGeneratorCommand $command)
+    {
+        $dialog = $this->getMock('Symfony\Component\Console\Helper\DialogHelper', array('ask'));
+        $dialog->expects($this->at(0))
+            ->method('ask')
+            ->will($this->returnValue('y'));
+        $command->getHelperSet()->set($dialog, 'dialog');
     }
 }
--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
-if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
+use Doctrine\Common\Annotations\AnnotationRegistry;
+if (!is_file($loaderFile = __DIR__.'/../vendor/autoload.php')) {
     throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
 }
-
-require $autoloadFile;
+$loader = require $loaderFile;
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
